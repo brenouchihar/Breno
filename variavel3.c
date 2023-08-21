ef8dfdf5d36f90b63c5769835053d3c224ea53f3
brenouchihar <142348444+brenouchihar@users.noreply.github.com>
2023-08-21 19:40:11 -0300

    Add files via upload

---
+++ variavel3.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+
+    main(){
+        char nome;
+        int idade;
+        int matrícula;
+        char endereço;
+        char curso;
+        char periodo;
+        char disciplina;
+        int mensalidade;
+
+        printf("Digite seu nome");
+        scanf("%[^\n]", matrícula);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        printf("Digite sua idade");
+
+        printf("Matrícula");
+
+        printf("enderço");
+
+        printf("curso");
+
+        printf("Periodo");
+
+        printf("Disciplina");
+
+        printf("Valor da mensalidade");
+
+        
+
+        scanf("%d", numero);
+
+
+    }
